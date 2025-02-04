03cea0938014e92427e45a33827d1a989825e77b
Tuva Gyllensten <tuvagyllensten@gmail.com>
2025-02-04 15:22:46 +0100

    added inmemory + nuget

---
+++ API/Data/AppDbContext.cs
@@ -0,0 +1,13 @@
+using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Data;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    {
+
+    }
+    
+}
