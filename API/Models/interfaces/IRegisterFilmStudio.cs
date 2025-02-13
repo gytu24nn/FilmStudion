31bd7f42b7a2caf70683c9b89393169745e8248b
Tuva Gyllensten <tuvagyllensten@gmail.com>
2025-02-13 14:07:37 +0100

    fixed name to filmstudioName

--- API/Models/interfaces/IRegisterFilmStudio.cs
+++ API/Models/interfaces/IRegisterFilmStudio.cs
@@ -4,7 +4,7 @@ namespace API.Models.interfaces;
 
 public interface IRegisterFilmStudio
 {
-    public string Name {get; set;}
+    public string FilmstudioName {get; set;}
     public string Email {get; set;}
     public string City {get; set;}
 }
